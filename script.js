ca047073c424a2b6033c351c8fe32dafd58ddd8e
umidjon <uziyatdinov@gmail.com>
2021-12-01 10:57:33 +0900

    i have modified script.js to make it clean

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-var input=document.getElementById("input");
-var button=document.getElementById("submit");
-var ul=document.querySelector("ul");
-var emoji = String.fromCodePoint(128204)
+const input=document.getElementById("input");
+const button=document.getElementById("submit");
+const ul=document.querySelector("ul");
+const emoji = String.fromCodePoint(128204)
 
-button.addEventListener("click", function () {
-    if (input.value.length > 0) {
-        var li=document.createElement("li");
-        li.appendChild(document.createTextNode(`${emoji} ${input.value}`));
-        ul.appendChild(li);
-        input.value= ""; }
-})
+//Check for input  length
+function isEmpty(){
+    if(input.value.length > 0){
+        return true;
+    }
+    return false;
+}
 
-input.addEventListener("keypress", function (event) {
-    if (input.value.length >0 && event.code === "Enter") {
-        var li=document.createElement("li");
+//Separation of concern. And Dont Repeat your code.
+//Function for adding text
+function addText(){
+    
+        //ES6 use let
+        let li=document.createElement("li");
+        //Ver good code. 
         li.appendChild(document.createTextNode(`${emoji} ${input.value}`));
         ul.appendChild(li);
         input.value= "";
-    }
-})
+}
 
+//function to add text when the button clicked
+function add_After_Click(){
+    if(isEmpty()){
+        addText();
+    }
+}
+
+//function to add text when enter clicked
+function add_after_enter(event){
+    if(isEmpty() && event.code === "Enter"){
+        addText();
+    }
+}
+
+//Adding after button clicked
+button.addEventListener("click", add_After_Click);
+
+//Adding after enter clicked
+input.addEventListener("keypress", add_after_enter);
